9b63424b9c4bc2a997e08c729ee0ac92905bc7a0
ImbuedF4 <rossco101@hotmail.com>
2021-03-13 17:15:45 +0000

    as

---
+++ app/src/main/java/spygame/assignRoles.java
@@ -0,0 +1,26 @@
+public class assignRoles {
+
+    int userID;
+    String lawyer;
+    String banker;
+    String artist;
+    String noble; 
+
+    assignRoles(int userID) {
+        this.userID = userID;
+    }
+
+    int getUserId() {
+        return userID;
+    }
+
+    
+    
+
+
+
+        
+}
+        
+
+
