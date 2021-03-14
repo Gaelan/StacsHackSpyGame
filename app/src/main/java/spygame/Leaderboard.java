0f43c212cdf7ebcfedc82097086790bd6b16fc4f
Gaelan Steele <gbs@canishe.com>
2021-03-14 01:05:16 -0800

    fix merge

--- app/src/main/java/spygame/Leaderboard.java
+++ app/src/main/java/spygame/Leaderboard.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package spygame;
 
 public class Leaderboard {
@@ -12,30 +11,3 @@ public class Leaderboard {
     }
 
 }
-=======
-//package spygame;
-//
-//public class Leaderboard {
-//
-//    String playerid;
-//    int playerscore;
-//
-//    public Leaderboard(String name, int score){
-//            playerid = name;
-//            playerscore = score;
-//    }
-//
-//    if(event.getSource()==newmatch)
-//
-//    {
-//        if (p1score != 0 || p2score != 0)
-//        {
-//            Leaderboard x;
-//
-//            if (p1score > p2score)
-//            {
-//                x = new Leaderboard (player1name, p1score);
-//            }
-//
-//}
->>>>>>> 947209235c7efc6d34e8ef8ce7a2e4254ebb0157
